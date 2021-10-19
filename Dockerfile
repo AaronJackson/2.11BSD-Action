fc13db0411a8ad188daaccd72415806f86dd685e
Aaron Jackson <aaron@aaronsplace.co.uk>
2021-10-20 00:31:14 +0100

    ftpd now running and can be connected to through nat
    Bit of a faff because there we can't use tun/tap inside the container
    (as this would require privileged mode). The simh nat stack works
    instead, but it also uses the same subnet as docker/podman
    (10.0.2.0/24). This is ok though, and actually useful in some ways,
    because it allows us to open an ACTIVE ftp session without trying to
    set the source address.

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,14 @@ FROM ubuntu:20.04
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt update && \
-    apt install -y simh expect curl libpcap-dev net-tools git build-essential
+    apt install -y expect curl libpcap-dev net-tools git \
+    build-essential netcat ftp
 
 RUN curl -so ci.dsk https://asjackson.s3.fr-par.scw.cloud/211bsd/ci.dsk
 
+RUN git clone https://github.com/simh/simh.git && \
+    cd simh && \
+    make pdp11 && cp BIN/pdp11 /pdp11
+
 COPY runpdp.sh /runpdp.sh
 RUN chmod +x /runpdp.sh
-
-RUN git clone https://github.com/simh/simh.git && \
-    cd simh && make pdp11 && cp BIN/pdp11 ./pdp11
-
-
-
--- runpdp.sh
+++ runpdp.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8)
+PASSWORD=Password9
 DATE=$(date +%y%m%d%H%M)
+GW=$(ifconfig tap0 | grep "inet 10.0" | awk '{ print $2 }')
 
-echo "Temporary password will be set to $PASSWORD"
+echo "2.11BSD root password will be set to $PASSWORD"
 echo "Date will be set to $DATE"
 
-cat > pdp.expect <<EOF
+cat - dump.expect > pdp.expect <<EOF
 #!/usr/bin/expect -f
 spawn /pdp11
 
-expect "sim>" {send "set cpu 11/73 4M\n"}
+expect "sim>" {send "set cpu 11/73 4M idle\n"}
 expect "sim>" {send "set rq enabled\n"}
 expect "sim>" {send "att rq0 ci.dsk\n"}
 expect "sim>" {send "set clk 50hz\n"}
-expect "sim>" {send "att xq nat:\n"}
+expect "sim>" {send "att xq nat:tcp=21:10.0.2.64:21,gateway=$GW\n"}
 expect "sim>" {send "boot rq0\n"}
 
 expect ": " {send "ra(0,0,0)unix\n"}
@@ -28,11 +29,34 @@ expect "# " {send "passwd\n"}
 expect "New password:" {send "$PASSWORD\n"}
 expect "Retype new password:" {send "$PASSWORD\n"}
 
-expect "# " {send "sleep 100\n"}
-interact
+expect "# " {send "echo 127.0.0.1 localhost > /etc/hosts\n"}
+expect "# " {send "echo 10.0.2.64 simh >> /etc/hosts\n"}
+expect "# " {send "> /etc/ftpusers\n"}
+expect "# " {send "echo ALL: ALL: ALLOW >> /etc/hosts.allow\n"}
+expect "# " {send "ifconfig qe0 inet netmask 255.255.255.0 simh broadcast 10.0.2.255 up\n"}
+
+set timeout -1
+expect "0123456789" {send "while true ; do sleep 100; done\n"}
 EOF
 
-/sbin/ifconfig tap0 up
-
 chmod +x pdp.expect
-./pdp.expect
+./pdp.expect &
+
+apt install -y ftp netcat
+
+while ! nc -z $GW 21 ; do
+  sleep 5
+done
+
+# Note about ftp - because we are using simh nat (because we don't
+# want privileged containers), the ftp connection has to "look like"
+# it's coming form a public address, so it can connect back. We set
+# the gateway in simh to be the docker address. The fact that the BSD
+# installation is in the same subnet is just coincidental and nothing
+# to worry about.
+
+# Verify ftp connection
+ftp -invp $GW <<EOF
+user root $PASSWORD
+ls
+EOF
