1f117c23825b3c9541bbd45d23609d76ae7bed26
Aaron Jackson <aaron@aaronsplace.co.uk>
2023-04-10 22:36:07 +0100

    fixup! expect rather than checkrun

--- runpdp.sh
+++ runpdp.sh
@@ -41,7 +41,7 @@ expect "# " {send \004}
 expect "login: " {send "root\n"}
 
 expect "# " {send "date $DATE\n"}
-expect "# " {send "sleep 3"\n"}
+expect "# " {send "sleep 3\n"}
 
 proc checkrun {cmd} {
   expect "# " { send "\$cmd\n" }
